d668acfbdf4ce0c1d31088d6311b6183ac007c5e
abhinav <abhinavpadige06@gmail.com>
2026-09-03 22:14:12 +0530

    Rewrite App.jsx with MonitorResults component

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,17 @@ import Hero from './components/Hero.jsx'
 import Features from './components/Features.jsx'
 import Architecture from './components/Architecture.jsx'
 import TechStack from './components/TechStack.jsx'
+import MonitorResults from './components/MonitorResults.jsx'
 import Footer from './components/Footer.jsx'
 
 export default function App() {
   return (
-    <div className="min-h-screen overflow-x-hidden">
+    <div className='min-h-screen bg-dark-900 text-white overflow-x-hidden'>
       <Hero />
       <Features />
       <Architecture />
       <TechStack />
+      <MonitorResults />
       <Footer />
     </div>
   )
